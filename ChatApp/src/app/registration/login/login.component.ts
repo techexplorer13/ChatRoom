7809936ae129fe4a40930607864757a2401d779c
arjshriv <arjitshrivastava@yahoo.in>
2020-12-14 16:41:40 +0530

    login changes and fixes

--- ChatApp/src/app/registration/login/login.component.ts
+++ ChatApp/src/app/registration/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private navCntrl:NavController) { }
 
   ngOnInit() {}
+
+  back(){
+    this.navCntrl.pop()
+  }
 
 }
